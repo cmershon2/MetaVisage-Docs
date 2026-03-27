e7bc38270c9c27626c7e4da48c90a482af9824af
Casey Mershon <caseymershon@gmail.com>
2026-03-27 12:53:39 -0400

    add: create getting started guide

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,11 +31,8 @@ export default defineConfig({
 			],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
+					label: 'Getting Started',
+					autogenerate: { directory: 'getting-started' },
 				},
 				{
 					label: 'Reference',
